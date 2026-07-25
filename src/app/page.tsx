555f4f9f6bb74c9c12ba5e7bafbfcf1e5aaaf759
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-07-25 20:30:56 +0300

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
+import {HomePage} from "@/src/components/home";
+
 export default function Home() {
-    return (
-        <div
-            className="
-                mx-auto
-                w-full
-                max-w-[1600px]
-                px-4
-                py-8
-                sm:px-5
-                md:px-6
-                lg:px-8
-            "
-        >
-            <h1 className="text-2xl font-semibold">
-                TradeScope
-            </h1>
-        </div>
-    );
+    return <HomePage />;
 }
---
+++ src/components/home/HomePage.tsx
@@ -0,0 +1,22 @@
+const HomePage = () => {
+    return (
+        <main
+            className="
+                mx-auto
+                w-full
+                max-w-[1600px]
+                px-4
+                py-8
+                sm:px-5
+                md:px-6
+                lg:px-8
+            "
+        >
+            <h1 className="text-2xl font-semibold">
+                TradeScope
+            </h1>
+        </main>
+    );
+};
+
+export {HomePage};
